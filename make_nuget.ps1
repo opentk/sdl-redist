1780774b2cd9390d84e27ba977e03ac131ae7d6a
Julius Häger <julius_hager@hotmail.com>
2025-10-19 16:30:13 +0200

    Update to last SDL 2.x version (2.32.6)

--- make_nuget.ps1
+++ make_nuget.ps1
@@ -2,8 +2,8 @@ param([String]$projectDir, [int]$verBuild)
 
 $ErrorActionPreference = "Stop"
 [int]$SDL_MAJOR_VERSION=2
-[int]$SDL_MINOR_VERSION=26
-[int]$SDL_PATCH_VERSION=5
+[int]$SDL_MINOR_VERSION=32
+[int]$SDL_PATCH_VERSION=6
 [String]$SDL_VERSION="$($SDL_MAJOR_VERSION).$($SDL_MINOR_VERSION).$($SDL_PATCH_VERSION)"
 
 [String]$SDL_SO_POSTFIX="0.$($SDL_MINOR_VERSION * 100).$($SDL_PATCH_VERSION)"
